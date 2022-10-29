ac2e3069d58c063338fa5b66951db80b76ce315d
Vinit Harsora <harsora.v@northeastern.edu>
2022-10-29 10:13:42 -0400

    Checking credentials

--- buildscript.sh
+++ buildscript.sh
@@ -8,8 +8,9 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/s
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install jenkins -y
+sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo systemctl is-enabled jenkins
+sudo systemctl enabled jenkins
 sudo apt install nginx -y
 nginx -v
 sudo systemctl status nginx
