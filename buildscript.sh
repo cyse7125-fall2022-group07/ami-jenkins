24ce4e832305a006ad56f8d0b13245c4a3c4b07e
Vinit Harsora <harsora.v@northeastern.edu>
2022-10-29 10:48:01 -0400

    Pushing initial changes

--- buildscript.sh
+++ buildscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install openjdk-11-jre -y
+sudo apt install openjdk-11-jdk -y
 sleep 10
 java -version
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
