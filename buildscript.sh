3abc02047c0ee7dd38ee216999cef5e2c79fa168
Vinit Harsora <harsora.v@northeastern.edu>
2022-11-15 10:08:41 -0500

    Installing Node.js

--- buildscript.sh
+++ buildscript.sh
@@ -3,6 +3,12 @@
 sudo apt update
 ############ Installing Java ##############
 sudo apt install openjdk-11-jre -y
+############ Installing Node ##############
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+nano nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
+node -v
 ############ Installing Jenkins ##############
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
