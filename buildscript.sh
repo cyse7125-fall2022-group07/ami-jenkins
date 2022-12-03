47758ead168b6480947cc2d91080b067d32eda2f
Shrawani <karpe.s@northeastern.edu>
2022-12-02 20:39:59 -0500

    install docker

--- buildscript.sh
+++ buildscript.sh
@@ -24,6 +24,6 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo chmod 666 /var/run/docker.sock
 
